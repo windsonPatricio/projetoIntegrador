a738b68bc3255e9732f9e936deca9aa13cebadd5
windsonPatricio <54744102+windsonPatricio@users.noreply.github.com>
2019-11-11 19:50:01 -0300

    adicao da entidade Agente

---
+++ loja/src/Entity/AgenteViagem.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Ifnc\Tads\Entity;
+
+
+use Ifnc\Tads\Helper\Record;
+
+class AgenteViagem extends Record
+{
+    public $id;
+    public $nome;
+    public $usuario;
+    public $senha;
+
+
+}
